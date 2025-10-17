d374d808c0d1b36b3decfe62c379397d9ca42f8f
akshaykhardekar10 <khardekarakshay33@gmail.com>
2025-10-18 01:49:43 +0530

    SEO improvements

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -101,7 +101,7 @@ const About = () => {
               <div className="relative">
                 <div className="w-64 h-64 md:w-80 md:h-80 rounded-full overflow-hidden border-4 border-white dark:border-gray-800 shadow-lg">
                   <img
-                    src="./src/public/AkshayProfile2.jpeg"
+                    src="/AkshayProfile2.jpeg"
                     alt="Profile"
                     className="w-full h-full object-cover"
                   />
